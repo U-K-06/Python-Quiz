63448093695d26f08a0738645aa430c4a5d1f796
U-K-06 <utkarsh7khajuria8@gmail.com>
2025-09-28 16:16:17 +0530

    added routing and started development of questions

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link,Routes } from 'react-router-dom'
+
 import dict from './assets/dict.svg'
 import string from './assets/string.svg'
 import list from './assets/list.svg'
 
 
-function App() { 
+function Landing() { 
   const total_options = 3
   const selected = new Array(total_options).fill(0)
   const [isSelected, setIsSelected] = useState(selected)
@@ -38,22 +39,21 @@ function App() {
 
       </div>
       <nav><a className='sm:text-lg lg:text-4xl text-shadow-primary font-bold text-shadow-lg' href='https://github.com/U-K-06' target='_blank'>U.K. </a></nav>
-      <h1 className='text-primary-text text-8xl text-centre font-bold text-center'>Quiz</h1>
+      <h1 className='text-primary-text text-6xl text-centre font-bold text-center'>Read to test your Python Knowledge?</h1>
       <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-15'>
         <li onClick={()=>selectTopic(0)} className={`${className} ${isSelected[0] ? '!bg-correct' : '!bg-primary'}`}><button className='text-3xl cursor-pointer'>Strings</button><img className='w-12 h-12 border-2' src={string} alt="string-icon" /></li>
         <li onClick={()=>selectTopic(1)} className={`${className} ${isSelected[1] ? '!bg-correct' : '!bg-primary'}`}><button className='cursor-pointer'>Lists</button><img src={list} alt="list-icon" /></li>
         <li onClick={()=>selectTopic(2)} className={`${className} ${isSelected[2] ? '!bg-correct' : '!bg-primary'}`}><button className='cursor-pointer'>Dictionaries</button><img src={dict} alt="dictionary-icon" /></li>
       </ul>
-      <a href="" className={className + ' bg-primary'}>Start the quiz!</a>
+      <Link to={'./Questions'} className={className + ' bg-primary'} state={{selectedCats:selected}}>Start the quiz!</Link>
 
       <footer><ul>
         <li></li>
         <li></li>
         <li></li>
       </ul></footer>
+      
     </>
-
   )
 }
-
-export default App
+export default Landing
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'; 
-import Landing from './Landing.jsx'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import  Landing  from './Landing.jsx'
+import Question from './Question.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>    
-    <Landing/> 
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Landing />} />
+    <Route path = "/Questions" element = {<Question/>} />
+  </Routes> 
     </BrowserRouter>
   </StrictMode>,
 )
