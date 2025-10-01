b2bee34df87e12aad06b083467bb7462f7ed7774
U-K-06 <utkarsh7khajuria8@gmail.com>
2025-10-02 01:10:42 +0530

    added basic design of question page and adeed sample questions

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,18 +1,28 @@
-function Question()
-{
-    return (
-        <>
-        <div className="bg-card-question">
-        <p className="sm:text-lg md:text-2xl lg:text-4x font-bold text-center p-6 m-20 w-11/12 md:w-3/4 lg:w-2/3 rounded-lg md:rounded-xl lg:rounded-2xl justify-self-center border-border text-text-primary">What is Your Name?</p>
-        <p className="border-primary border-l-8 bg-text-secondary justify-self-center">Explanation of answer</p>
-        <ul className="flex flex-col items-center justify-evenly h-screen">
-            <li>Pak</li>
-            <li>Paku</li>
-            <li>Pakuman</li>
-            <li>Paktheduck</li>
+function Question() {
+const className =
+  "bg-card-option-default rounded-2xl text-4xl w-11/12 text-center h-14  flex items-center justify-center hover:bg-gray-950";
+
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-background p-4">
+      <div className="bg-card-question border border-border rounded-2xl w-full max-w-4xl p-8 space-y-8">
+        <p className="text-center text-text-primary font-bold text-2xl sm:text-3xl lg:text-4xl">
+          What is Your Name?
+        </p>
+
+        <p className="border-l-8 border-primary bg-text-secondary text-text-primary p-4 rounded-md text-2xl">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati repellendus architecto illum quisquam quo saepe beatae. Sed excepturi repellat ex, ipsam assumenda incidunt nostrum sunt sit neque officia nemo nisi?
+          Excepturi beatae modi ipsa illum nam aliquam, eos consequatur quaerat iure illo itaque consequuntur magnam sequi accusamus voluptates in unde soluta saepe iste laudantium totam facere, magni velit? Exercitationem, magnam?
+        </p>
+
+        <ul className="flex flex-col items-center space-y-6">
+          <li className={className}>a</li>
+          <li className={className}>b</li>
+          <li className={className}>c</li>
+          <li className={className}>d</li>
         </ul>
-        </div>
-        </>
-    )
+      </div>
+    </div>
+  );
 }
-export default Question
+export default Question;
